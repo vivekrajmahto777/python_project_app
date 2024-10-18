1ca8711715be2ddf9b5e773f3452f70db1c78af3
vivekrajmahto777 <vivekrajmahto1608@gmail.com>
2024-10-18 14:02:22 +0530

    first commit

---
+++ test_app.py
@@ -0,0 +1,19 @@
+#test_app.py
+
+import pytest
+from app import create_app
+from urllib.parse import quote_plus
+
+@pytest.fixture
+def app():
+    return create_app()
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+def test_home(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    expected_text = 'Hello World'
+    assert expected_text.encode() in response.data
