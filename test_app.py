f825393845d5fd7b8e855b3dc6be13628e7b77b7
vivekrajmahto777 <168958925+vivekrajmahto777@users.noreply.github.com>
2024-12-08 20:24:58 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -15,5 +15,5 @@ def client(app):
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    expected_text = 'Hey welcome to my sample python-flask Application:0.0.1'
+    expected_text = 'Hey welcome to my sample python-flask Application Version:0.0.1 latest :-('
     assert expected_text.encode() in response.data
